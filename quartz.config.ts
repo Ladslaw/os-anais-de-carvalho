6f9fc109a5b2774f286242d84093fe17af87e229
Ladslaw <142825934+Ladslaw@users.noreply.github.com>
2025-04-29 14:34:51 -0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://ladslaw.github.io/os-anais-de-carvalho",
+    baseUrl: "https://ladslaw.github.io/os-anais-de-carvalho/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
